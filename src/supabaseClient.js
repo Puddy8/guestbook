806d181b8b936ebe0a983bad47215c15514777cf
Patrick Thelen <patrick@Patricks-MacBook-Pro-2.local>
2025-08-02 14:38:59 +0200

    Update Supabase URL and anon key to correct project

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // These keys connect your app to *your* Supabase backend
-const supabaseUrl = 'https://kvljzprmmvrdzyeuvwbt.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2bGp6cHJtbXZyZHp5ZXV2d2J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4MDI2MDcsImV4cCI6MjA2ODM3ODYwN30.WzA2lBsUdoQ-tyFr5jV-7Jo6w0AHS6_C1Q3_x0Dbss4';
+const supabaseUrl = 'https://swnwnnguwglyuiuvtvda.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3bndubmd1d2dseXVpdXZ0dmRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5NDE4NjgsImV4cCI6MjA2ODUxNzg2OH0.K41CPxPlkIAGYoG6Oh75t8aQfSI66hGJ76WReC2ltSo';
 
 // Create a single Supabase client that the whole app can use
 export const supabase = createClient(supabaseUrl, supabaseKey);
